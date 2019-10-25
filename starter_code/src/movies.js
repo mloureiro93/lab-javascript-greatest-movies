1bd63617afde4ab1b9fa165ed2efe3c209c69957
mloureiro <mloureiro@mloureiro.localdomain>
2019-10-25 09:02:27 +0100

    1 to 5

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,30 +2,24 @@
 
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals
 
-function calculateAverageMovieRate (moviesArray) {
-    let ratesCount = 0;
-     for (let i = 0; i < moviesArray.length; i++)
-      {
-        ratesCount += moviesArray[i].rate;
-      
-    
-};
-let ratesDivided = ratesCount/arrayOfMovies.length;
-    return ratesDivided
+function calculateAverageMovieRate (array){ 
+    let rates = array.reduce((accumalator, value) => { 
+    return accumalator + value["rate"]; 
+    },0); 
+    let average = rates/array.length; 
+    return average;
 }
-      
-     
-
+calculateAverageMovieRate(MOVIES);
 
 // Iteration 2: Drama movies - Get the average of Drama Movies
-
-function calculateAverageDramaRate (array) {
-const dramaArray = array.filter((movie) => {
-  return movie.genre.includes("Drama") 
-       
-    })
-  return calculateAverageMovieRate(dramaArray) || 0
-  };
+function calculateAverageDramaRate(newArray) {
+    const filteredArray = newArray.filter((value) => {
+        return value.genre.includes("Drama")
+    }) 
+  return calculateAverageMovieRate(filteredArray) || 0; 
+}; 
+ 
+ calculateAverageMovieRate(MOVIES); 
 
   
 
@@ -51,16 +45,45 @@ function orderByYear (array) {
      
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
-function countSpielbergDramaMovies (arraySpilb) {
-    let spilbMovies = arraySpilb 
-
+function countSpielbergDramaMovies (arraySpilb){
+    const stevenArray = arraySpilb.filter((value) => {
+        if(value.director === 'Steven Spielberg' && value.genre.includes("Drama")) {
+            return true;
+        } else {
+     return false;
 }
-
-
+    });
+    return stevenArray.length;
+}
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically (array) { 
+    array.sort((a, b) => {
+        if(a.title > b.title) { 
+            return 1;
+            } else if (a.title < b.title) { 
+            return -1;
+            } else {
+            return 0;
+            } 
+    }); 
+        const titles = array.map((value) => { 
+            return value.title
+        }); 
+        const topTwenty = titles.slice(0, 20); 
+        return topTwenty; 
+    }
+    
+    
+    
+    
+    
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+
+function turnHoursToMinutes (array) {
+
+}
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
 
